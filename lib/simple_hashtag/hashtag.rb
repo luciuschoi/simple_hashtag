107b7ff9b46836f310ad0aa1c9b340ee1c4f6650
Koding Doc <lucius.choi@gmail.com>
2017-02-06 14:09:21 +0900

    Update fixing unicode characters
    https://searchcode.com/codesearch/raw/90922615/

--- lib/simple_hashtag/hashtag.rb
+++ lib/simple_hashtag/hashtag.rb
@@ -9,13 +9,38 @@ module SimpleHashtag
     # TODO Beef up the regex (ie.:what if content is HTML)
     # this is how Twitter does it:
     # https://github.com/twitter/twitter-text-rb/blob/master/lib/twitter-text/regex.rb
-    HASHTAG_REGEX = /(?:\s|^)(#(?!(?:\d+|\w+?_|_\w+?)(?:\s|$))([a-z0-9가-힣\-_]+))/i
+    HASHTAG_REGEX = /(?:\s|^)(#(?!(?:\d+|\w+?_|_\w+?)(?:\s|$))([a-z0-9\-_]+))/i
 
     def self.find_by_name(name)
       Hashtag.where("lower(name) =?", name.downcase).first
     end
     def self.find_or_create_by_name(name, &block)
       find_by_name(name) || create(name: name, &block)
+    end
+
+    ### SCOPES:
+
+    def self.named(name)
+      where(["LOWER(name) = LOWER(?)", as_8bit_ascii(unicode_downcase(name))])
+    end
+
+    def self.named_any(list)
+      clause = list.map { |tag|
+        sanitize_sql(["LOWER(name) = LOWER(?)", as_8bit_ascii(unicode_downcase(tag))])
+      }.join(" OR ")
+      where(clause)
+    end
+
+    def self.named_like(name)
+      clause = ["name #{like_operator} ? ESCAPE '!'", "%#{escape_like(name)}%"]
+      where(clause)
+    end
+
+    def self.named_like_any(list)
+      clause = list.map { |tag|
+        sanitize_sql(["name #{like_operator} ? ESCAPE '!'", "%#{escape_like(tag.to_s)}%"])
+      }.join(" OR ")
+      where(clause)
     end
 
 
@@ -56,6 +81,34 @@ module SimpleHashtag
       # TODO Make this method call a single SQL query
       orphans = self.all.select { |h| h.hashtaggables.size == 0 }
       orphans.map(&:destroy)
+    end
+
+    class << self
+      private
+
+      def comparable_name(str)
+        unicode_downcase(str.to_s)
+      end
+
+      def binary
+        using_mysql? ? "BINARY " : nil
+      end
+
+      def unicode_downcase(string)
+        if ActiveSupport::Multibyte::Unicode.respond_to?(:downcase)
+          ActiveSupport::Multibyte::Unicode.downcase(string)
+        else
+          ActiveSupport::Multibyte::Chars.new(string).downcase.to_s
+        end
+      end
+
+      def as_8bit_ascii(string)
+        if defined?(Encoding)
+          string.to_s.dup.force_encoding('BINARY')
+        else
+          string.to_s.mb_chars
+        end
+      end
     end
 
   end
